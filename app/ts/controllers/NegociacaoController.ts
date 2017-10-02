1e1563ea4746eeceb5f058aca4c33e6c4e724c51
tirodrigues <tirodrigues.jpa@gmail.com>
2017-10-02 12:56:35 -0300

    ajuste para view

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,11 +2,14 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -17,6 +20,7 @@ class NegociacaoController {
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value) );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
