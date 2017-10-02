94d6a4baf2ca92336b81769ede3c57000e0c0b70
tirodrigues <tirodrigues.jpa@gmail.com>
2017-10-02 17:51:40 -0300

    refactor

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,20 @@
 export abstract class View<T> {
 
     protected _elemento: JQuery;
+    private _escapar: boolean;
     
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar: boolean = false) {
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
 
     update(modelo: T ): void {
-        this._elemento.html(this.template(modelo));
+        let template = this.template(modelo);
+
+        if(this._escapar)
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+
+        this._elemento.html(template);
     }
 
     abstract template(modelo: T): string 
